74bde2a8b924ca8ea58d7923b6a6b8baa728f14c
Iswar <rick731202@gmail.com>
2023-03-03 23:05:50 +0530

    Hero section done

---
+++ src/lib/components/HomePageHeroSection.svelte
@@ -0,0 +1,32 @@
+<script>
+	import ChromeLogo from '$lib/images/icons/chrome_logo.png';
+	import PlayIcon from '$lib/images/icons/play-icon.svg';
+</script>
+
+<div class="h-[80vh] flex justify-center items-center border-2 hero-bg-image">
+	<div class="w-[30%] mr-12">
+		<h1 class="text-h1 my-6">Cyber Security Meets Web3</h1>
+		<p class="text-p my-6">
+			NetSepio gives you the best internet protection from scams and malware leveraging the power of
+			Blockchain.
+		</p>
+		<div class="flex items-center">
+			<button class="btn-secondary"
+				>Download for <span class="btn-right-icon"><img src={ChromeLogo} alt="chrome-logo" /></span
+				></button
+			>
+			<button class="btn-primary"
+				><span class="btn-left-icon"><img src={PlayIcon} alt="chrome-logo" /></span>Watch
+			</button>
+		</div>
+	</div>
+	<div class="w-[40%] h-[85%] ml-16 border-2 border-black flex justify-center items-center">
+		<h1>Video goes here</h1>
+	</div>
+</div>
+
+<style>
+	.hero-bg-image {
+		background-image: url('/src/lib/images/hero_section_background.png');
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
-Hola
-<button class="btn-secondary">Button <span class="btn-right-icon">Icon</span></button>
-<button class="btn-secondary"><span class="btn-left-icon">Icon</span>btn</button>
-<button class="btn-primary">Button</button>
-<div class="card">Lorem ipsum dolor sit amet.</div>
-<div class="card-secondary">Lorem ipsum dolor sit amet.</div>
-<div class="tabs-primary">Lorem ipsum dolor sit amet.</div>
+<script>
+	import HomePageHeroSection from '$lib/components/HomePageHeroSection.svelte';
+</script>
+
+<HomePageHeroSection />
