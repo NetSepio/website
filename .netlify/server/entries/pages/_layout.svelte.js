5488c310a53999a5df0c262f0f39f758059d5949
Aditya Mishra <adimis.ai.001@gmail.com>
2023-03-20 14:05:22 +0530

    Delete .netlify directory

--- .netlify/server/entries/pages/_layout.svelte.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/index.js";
-const app = "";
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div>${slots.default ? slots.default({}) : ``}</div>`;
-});
-export {
-  Layout as default
-};
--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const file = '_app/immutable/entry/_layout.svelte.9ae42be0.js';
-export const imports = ["_app/immutable/entry/_layout.svelte.9ae42be0.js","_app/immutable/chunks/index.fc291d87.js"];
-export const stylesheets = ["_app/immutable/assets/_layout.6bd37dca.css"];
-export const fonts = [];
