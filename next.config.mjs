06dfa5fe266c259bec3f674272503eea4da7524c
Ovodo <ovdizzle@gmail.com>
2024-05-16 11:13:58 +0100

    set images.unoptimized to true

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // <=== enables static exports
-  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
