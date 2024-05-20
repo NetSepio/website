0cf64baf48278c4f583dda4eb40f7f942be1d970
Ovodo <ovdizzle@gmail.com>
2024-05-21 00:03:21 +0100

    changed nextconfig

--- src/components/FooterItem.jsx
+++ src/components/FooterItem.jsx
@@ -1,20 +1,27 @@
 import useFonts from "@/hooks/useFonts";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const FooterItem = ({ item }) => {
   const { roboto } = useFonts();
   return (
-    <div style={roboto.style} className='w-max  px-[20px]'>
+    <div style={roboto.style} className='w-[200px] px-[20px]'>
       <h4 className=' text-white text-[22px] mb-[49px]'>{item.title}</h4>
       {item.content.map((content, index) => {
         return (
           <Link
             key={index.toString()}
             href={content.link}
-            className='text-white hover:text-cta duration-200 block my-2 font-thin'
+            className='text-white flex hover:text-cta duration-200 gap-2 hover:gap-3 my-2 font-thin'
           >
-            {content.name}
+            <p>{content.name}</p>
+            <Image
+              src={"images/arrow.svg"}
+              width={12}
+              height={12}
+              alt='arrow'
+            />
           </Link>
         );
       })}
