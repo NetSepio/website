3d11a3365047a0cb056dd071b3c920f12c4fccf8
Devsi <grace.emily876@gmail.com>
2024-07-25 17:30:27 +0530

    changes

--- src/app/extension/page.jsx
+++ src/app/extension/page.jsx
@@ -17,11 +17,11 @@ const Index = () => {
           return <FeatureItem item={item} key={index.toString()} />;
         })}
       </section>
-      <section className='w-[90%] mt-[5%] lg:mt-[10%] space-y-[5vh] lg:pace-y-[10vh] flex flex-col items-center justify-center h-[542px] bg-cover bg-[url(/images/extension/install_bg.svg)] bg-no-repeat lg:h-[724px]'>
+      <section className='w-[90%] mt-[5%] lg:mt-[10%] space-y-[5vh] lg:pace-y-[10vh] flex flex-col items-center justify-center h-[542px] bg-cover bg-[url(/images/extension/install_bg.png)] bg-no-repeat lg:h-[724px]'>
         <h2 className='text-2xl text-white font-semibold md:text-6xl  '>
           NetSepio Browser Extension
         </h2>
-        <p className='bg-gradient-to-r from-primary to-[#3185FC] from-0% via-90% to-100% via-[#91A6FF] bg-clip-text text-transparent font- text-lg lg:text-3xl'>
+        <p className='text-lg lg:text-3xl'>
           Don&apos;t just browse the web—browse it better.
         </p>
         <CustomBtn
