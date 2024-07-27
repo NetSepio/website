eb268563abcc4d7cb11fda81c1d66a6bc59ba4b0
Devsi <grace.emily876@gmail.com>
2024-07-27 13:58:27 +0530

    mobile app coming soon

--- src/utils/data.js
+++ src/utils/data.js
@@ -25,8 +25,8 @@ export const cardDetails = [
   {
     image: "pix_4",
     title: "Mobile App",
-    link: "https://drive.google.com/file/d/1jdkqwIXY825iG3shRRoFyg5SMGy6LN8J/view?usp=drive_link",
-    button: "Download now",
+    link: "#",
+    button: "Coming soon",
 
     desc: "Empower your online activity with our Erebrus mobile app for secure, private and accessible network. We utilize NFTs for robust privacy and decentralization.",
   },
