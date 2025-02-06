ce6f64825fdd0f5130c0bf2a27a748c200727334
Jatin Yadav <jatin27yadav@gmail.com>
2025-02-06 23:08:12 +0530

    [FEAT]: Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const Hero = () => {
+  return (
+    <>
+      <div
+        className="hero-container"
+        style={{
+          position: "relative",
+          height: "100vh",
+        }}
+      >
+        <NavBar />
+        <div
+          style={{
+            backgroundImage: "url('/images/background_image.jpeg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "100%",
+            width: "100%",
+            filter: "brightness(35%)",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        ></div>
+
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "8%",
+            transform: "translateY(-50%)",
+            color: "white",
+            textAlign: "left",
+            fontSize: "40px",
+            fontWeight: "400",
+          }}
+        >
+          <h1>
+            DePIN Powered Private, <br /> Secure & Agentic Internet
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              gap: "2rem",
+            }}
+          >
+            <button
+              style={{
+                padding: "15px 30px",
+                border: "2px solid #11D9C5",
+                backgroundColor: "transparent",
+                color: "white",
+                borderRadius: "10px",
+                fontSize: "1rem",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              Follow on X
+            </button>{" "}
+            <button
+              style={{
+                padding: "15px 30px",
+                border: "2px solid #11D9C5",
+                backgroundColor: "transparent",
+                color: "white",
+                borderRadius: "10px",
+                fontSize: "1rem",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              Join Discord
+            </button>
+          </div>
+        </div>
+
+        <div style={{ position: "absolute", bottom: "0", width: "100%" }}>
+          <div
+            style={{
+              background:
+                "linear-gradient(90deg, rgba(5, 11, 33, 0.5), rgba(11, 114, 115, 0.5), rgba(5, 11, 33, 0.5))",
+              height: "100px", // Adjust height as needed
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "white",
+              fontSize: "30px",
+              textAlign: "center",
+              fontWeight: "bold",
+            }}
+          >
+            <div>
+              DVPN and AI Coordination Layer for Privacy and Sovereignty
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div
+      style={{
+        backgroundColor: "#040A20",
+        padding: "0 8rem"
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: "100px",
+        }}
+      >
+        <div>
+          <img
+            src="/images/Logo.png"
+            alt="NetSepio-Logo"
+            style={{
+              height: "45px",
+            }}
+          />
+          <img
+            src="/assets/NetSepio.svg"
+            alt="NetSepio-Logo"
+            style={{
+              height: "35px",
+              
+            }}
+          />
+        </div>
+        <div>
+          {/* Explore Button */}
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            style={{
+                padding: "15px 30px",
+                border: "2px solid #11D9C5",
+                backgroundColor: "transparent",
+                color: "white",
+                borderRadius: "10px",
+                fontSize: "1rem",
+                fontWeight: "bold",
+                cursor: "pointer"
+            }}
+          >
+            Explore
+          </button>
+
+          {/* Dropdown Menu */}
+          {isOpen && (
+            <div
+              style={{
+                position: "absolute",
+                top: "75px",
+                right: "31px",
+                backgroundColor: "white",
+                boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
+                overflow: "hidden",
+                width: "150px",
+              }}
+            >
+              <ul style={{ listStyle: "none", margin: 0, padding: 0 }}>
+                {["Link1", "Link2", "Link3"].map((link, index) => (
+                  <li key={index} style={{ borderBottom: "1px solid #ddd" }}>
+                    <a
+                      href="#"
+                      style={{
+                        display: "block",
+                        padding: "10px",
+                        textDecoration: "none",
+                        color: "#333",
+                      }}
+                    >
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
