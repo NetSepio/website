f09f0c2def0519540dcdcc1ecd64281deb256348
Jatin Yadav <jatin27yadav@gmail.com>
2025-02-08 21:04:29 +0530

    [CHG]: Updated Explore Button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,28 +2,47 @@ import React, { useState } from "react";
 import "../styles/Navbar.css"; // Import the external CSS file
 
 const NavBar = () => {
-  const [isOpen] = useState(false);
-
+  const [isOpen, setOpenState] = useState(false);
+  const dropDownLinks = [
+    "https://erebrus.io/",
+    "https://www.cyreneai.com/",
+    "https://chromewebstore.google.com/detail/netsepio/bbkfclgnbddljhepbfpongcollhocghd",
+  ];
   return (
     <div className="navbar-container">
       <div className="navbar-content">
         <div className="navbar-logo">
-          <img src="/images/Logo.png" alt="NetSepio-Logo" className="logo-img" />
-          <img src="/assets/NetSepio.svg" alt="NetSepio-Logo" className="netsepio-logo" />
+          <img
+            src="/images/Logo.png"
+            alt="NetSepio-Logo"
+            className="logo-img"
+          />
+          <img
+            src="/assets/NetSepio.svg"
+            alt="NetSepio-Logo"
+            className="netsepio-logo"
+          />
         </div>
         <div className="navbar-menu">
-          <button className="explore-button">Explore</button>
+          <button
+            onClick={() => setOpenState(!isOpen)}
+            className="explore-button"
+          >
+            Explore
+          </button>
 
           {isOpen && (
             <div className="dropdown-menu">
               <ul className="dropdown-list">
-                {["Link1", "Link2", "Link3"].map((link, index) => (
-                  <li key={index} className="dropdown-item">
-                    <a href="https://google.com" className="dropdown-link">
-                      {link}
-                    </a>
-                  </li>
-                ))}
+                {["Erebrus", "CyreneAI", "Browser Extension"].map(
+                  (link, index) => (
+                    <li key={index} className="dropdown-item">
+                      <a href={dropDownLinks[index]} target="_blank" className="dropdown-link">
+                        {link}
+                      </a>
+                    </li>
+                  )
+                )}
               </ul>
             </div>
           )}
