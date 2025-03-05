7d3ebcefc2b469f2842c3ebffd79c7983e281b32
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2025-03-05 23:12:13 +0530

    Update Connectivity.jsx

--- src/components/Connectivity.jsx
+++ src/components/Connectivity.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Connectivity.css"; // Import the external CSS file
-import { FadeUp, ZoomIn } from "./FadeAnimations";
+import { ZoomIn } from "./FadeAnimations";
 
 const Connectivity = () => {
   return (
